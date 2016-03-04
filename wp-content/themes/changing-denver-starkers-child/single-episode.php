9bd4d2e9de4a1df3e9f00ff6d65e2f0d5b959507
Jeremy Zornow <jeremy@zornow.com>
2016-03-03 23:13:05 -0700

    Adds content wrapper (for max-width and margins) to single-ep

--- wp-content/themes/changing-denver-starkers-child/single-episode.php
+++ wp-content/themes/changing-denver-starkers-child/single-episode.php
@@ -11,30 +11,32 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div class="content">
+  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-  <article class="post">
-    <?php if ( has_post_thumbnail() ) { 
-      $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-      <div class="post-header episode-header" style="background-image:url('<?php echo $thumbnail_url ?>')">
-    <?php } else { ?>
-      <div class="post-header episode-header">
-    <?php } ?> 
-    <h2 class="post-title">
-      <a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark">
-        <?php the_title(); ?>
-      </a>
-    </h2>
-    <time class="post-date" datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate>
-      <?php the_date(); ?> <?php the_time(); ?>
-    </time>
-  </div> 
-  <iframe class="player" style="border: none; width:100%" src="//html5-player.libsyn.com/embed/episode/id/<?php echo the_field('episode_id'); ?>/height/46/width/640/theme/standard/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/backward/no-cache/true/" height="46" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-  <div class="post-body">
-    <?php the_content(); ?>
-  </div>
-  </article>
+    <article class="post">
+      <?php if ( has_post_thumbnail() ) { 
+        $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+        <div class="post-header episode-header" style="background-image:url('<?php echo $thumbnail_url ?>')">
+      <?php } else { ?>
+        <div class="post-header episode-header">
+      <?php } ?> 
+      <h2 class="post-title">
+        <a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark">
+          <?php the_title(); ?>
+        </a>
+      </h2>
+      <time class="post-date" datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate>
+        <?php the_date(); ?> <?php the_time(); ?>
+      </time>
+    </div> 
+    <iframe class="player" style="border: none; width:100%" src="//html5-player.libsyn.com/embed/episode/id/<?php echo the_field('episode_id'); ?>/height/46/width/640/theme/standard/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/backward/no-cache/true/" height="46" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+    <div class="post-body">
+      <?php the_content(); ?>
+    </div>
+    </article>
 
-<?php endwhile; ?>
+  <?php endwhile; ?>
+</div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
