cb687ce0d7d5671992fe37d372a3e430a263853c
Jeremy Zornow <jeremy@zornow.com>
2016-03-09 22:40:57 -0700

    Adds column display to episode template.

--- wp-content/themes/changing-denver-starkers-child/single-episode.php
+++ wp-content/themes/changing-denver-starkers-child/single-episode.php
@@ -35,9 +35,31 @@
         </time>
       </div> 
       <iframe class="player" style="border: none; width:100%" src="//html5-player.libsyn.com/embed/episode/id/<?php echo the_field('episode_id'); ?>/height/46/width/640/theme/standard/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/backward/no-cache/true/" height="46" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-      <div class="post-body">
-        <?php the_content(); ?>
+      
+        <?php 
+          if ( has_post_thumbnail() ) { 
+            $thumbnail_id = get_post_thumbnail_id($post->ID);
+            $thumbnail_url = wp_get_attachment_url( $thumbnail_id );
+          ?>
+          <div class="post-body post-body-with-columns">
+            <div class="post-thumbnail-holder">
+              <a class="post-thumbnail-link" href="<?php echo $thumbnail_url ?>">
+                <img class="post-thumbnail" src="<?php echo $thumbnail_url ?>">
+              </a>
+              <?php if ( has_excerpt($thumbnail_id) ) { ?>
+                <div class="post-thumbnail-caption">
+                  <?php echo get_post($thumbnail_id)->post_excerpt; ?>
+                </div>
+              <?php } ?>
+            </div>
+        <?php } else { ?>
+          <div class="post-body">
+        <?php } ?> 
+        <div class="post-description">
+          <?php the_content(); ?>
+        </div>
       </div>
+
       </article>
 
     <?php endwhile; ?>
