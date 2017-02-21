1a5527e631f6fedc394666c32f062b6de94ac71b
Jeremy Zornow <jeremy@zornow.com>
2017-02-20 18:15:16 -0700

    Create seasons
    Creates custom post type, custom fields on the episode object,  and seasons-single.php
    
    No archive will be required for our purposes.

--- wp-content/themes/changing-denver-starkers-child/functions.php
+++ wp-content/themes/changing-denver-starkers-child/functions.php
@@ -47,4 +47,95 @@ function create_episodes_post_type() {
   register_post_type( 'episode', $args );
 }
 
+add_action( 'init', 'create_seasons_post_type' );
+function create_seasons_post_type() {
+  $labels = array(
+    'name'               => _x( 'Seasons', 'post type general name' ),
+    'singular_name'      => _x( 'Season', 'post type singular name' ),
+    'menu_name'          => _x( 'Seasons', 'admin menu' ),
+    'name_admin_bar'     => _x( 'Season', 'add new on admin bar' ),
+    'add_new'            => _x( 'Add New', 'season' ),
+    'add_new_item'       => __( 'Add New Season' ),
+    'new_item'           => __( 'New Season' ),
+    'edit_item'          => __( 'Edit Season' ),
+    'view_item'          => __( 'View Season' ),
+    'all_items'          => __( 'All Seasons' ),
+    'search_items'       => __( 'Search Seasons' ),
+    'parent_item_colon'  => __( 'Parent Seasons:' ),
+    'not_found'          => __( 'No seasons found.' ),
+    'not_found_in_trash' => __( 'No seasons found in Trash.' )
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'has_archive' => true,
+    'rewrite' => array('slug' => 'seasons'),
+    'supports' => array( 'title' )
+  );
+
+  register_post_type( 'season', $args );
+}
+
+// Custom Field Definitions for Episodes
+
+if(function_exists("register_field_group"))
+{
+  register_field_group(array (
+    'id' => 'acf_episode-information',
+    'title' => 'Episode Information',
+    'fields' => array (
+      array (
+        'key' => 'field_56b055ae251a6',
+        'label' => 'Episode ID',
+        'name' => 'episode_id',
+        'type' => 'number',
+        'instructions' => 'The ID for this episode in LibSyn.',
+        'required' => 1,
+        'default_value' => '',
+        'placeholder' => '',
+        'prepend' => '',
+        'append' => '',
+        'min' => '',
+        'max' => '',
+        'step' => '',
+      ),
+      array (
+        'key' => 'field_58a12cec344ed',
+        'label' => 'Season Number',
+        'name' => 'season_number',
+        'type' => 'post_object',
+        'required' => 1,
+        'post_type' => array (
+          0 => 'season',
+        ),
+        'taxonomy' => array (
+          0 => 'all',
+        ),
+        'allow_null' => 0,
+        'multiple' => 0,
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'episode',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'acf_after_title',
+      'layout' => 'default',
+      'hide_on_screen' => array (
+      ),
+    ),
+    'menu_order' => 0,
+  ));
+}
+
+
 ?>
---
+++ wp-content/themes/changing-denver-starkers-child/single-season.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * The main template file
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file 
+ *
+ * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
+ *
+ * @package   WordPress
+ * @subpackage  Starkers
+ * @since     Starkers 4.0
+ */
+?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+<div class="content">
+  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <div class="page-title-block">
+      <h1 class="page-title">
+        <?php echo "Episodes - Season " . get_the_title(); ?>
+      </h1>
+    </div>
+    <?php 
+      // because seasons are named for their number
+      $season_id = get_the_ID();
+
+      $the_query = new WP_Query( array(
+        'post_type' => 'episode',
+        'paged' => $paged,
+        'meta_key'   => 'season_number',
+        'meta_value' => $season_id
+      ));
+      
+      if ( $the_query->have_posts() ):
+    ?>
+        <ol class="posts-list">
+      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <li class="post">
+          <article class="post-content">
+          <?php 
+            if ( has_post_thumbnail() ) { 
+              $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); 
+          ?>
+            <div class="post-header episode-header" style="background-image: url('<?php echo $thumbnail_url; ?>');">
+          <?php } else { ?>
+            <div class="post-header episode-header">
+          <?php } ?>
+              <h2 class="post-title">
+                <a href="<?php esc_url( the_permalink() ); ?>" title="<?php the_title(); ?>" rel="bookmark">
+                  <?php the_title(); ?>
+                </a>
+              </h2>
+              <time class="post-date" datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate>
+                <?php the_date(); ?>
+              </time> 
+            </div>
+            <iframe class="player" style="border: none; width:100%" src="//html5-player.libsyn.com/embed/episode/id/<?php echo the_field('episode_id'); ?>/height/46/width/640/theme/standard/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/backward/no-cache/true/" height="46" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+            <?php echo apply_filters('the_content', $mycontent); ?>
+          </article>
+        </li>
+      <?php endwhile; ?>
+    </ol>
+  <?php else: ?>
+    <h2>No episodes to display.</h2>
+  <?php endif; ?>
+
+
+  <?php endwhile; ?>
+</div>
+
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
